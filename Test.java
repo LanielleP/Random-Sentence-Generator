46e51692f792b7816877c9f38cbc16984281ebb6
Aaron Ba <61670781+ayayrom@users.noreply.github.com>
2021-10-05 17:41:29 +0000

    hell

---
+++ Test.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Test
+{
+  public static void main(String[] args)
+  {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    System.out.println("test");
+    String[] hello = new String[5];
+
+    hello[0] = "LMAO";
+    System.out.println(hello[0]);
+  }
+}
+
