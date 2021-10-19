df1f8c1859453a94570e9d6e022d4e9e39a65c01
Lani Pav <70656068+LanielleP@users.noreply.github.com>
2021-10-19 17:46:26 +0000

    10/19/21 updates to plan B

--- PlanB.java
+++ PlanB.java
@@ -7,3 +7,12 @@ public class PlanB
     System.out.println("test");
   }
 }
+
+//https://replit.com/talk/learn/Get-started-with-Web-Scraping/8930
+//https://replit.com/@jajoosam/quote-scraper-starter
+
+/* 
+STEP 1: pull info from a website
+STEP 2: make a complete sentence
+STEP 3: make a story
+*/
