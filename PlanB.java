51860abe37725520fb152c7e0206b6f48090131b
Jomar Jocson <70657450+JomarJocson@users.noreply.github.com>
2021-10-22 17:25:27 +0000

    Adverbs and Adjectives txt files created, added more suggestions to Step 2

--- PlanB.java
+++ PlanB.java
@@ -31,9 +31,10 @@ STEP 1: pull info from a website --
 
 STEP 2: make a complete sentence
       2a - Randomize structure purpose (i.e. Statements, Questions, Exclamations) 
-      2b - Simple Sentence Structure (i.e. Independent Clause, Object, Modifier (aka Subject Verb (maybe Direct Object)))
-      2c - Complex Sentence Structure (i.e. Dependent clause, Independent Clause (Combine two sentences with commas))
-      2d - Semi-colons.
+      2b - We can save a headache via making the user declare past/present/future tense beforehand
+      2c - Simple Sentence Structure (i.e. Independent Clause, Object, Modifier (aka Subject Verb (maybe Direct Object)))
+      2d - Complex Sentence Structure (i.e. Dependent clause, Independent Clause (Combine two sentences with commas))
+      2e - Semi-colons.
 
 STEP 3: make a story
 */
