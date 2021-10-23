d8ebbe71ec8c3ca0637085ed915a58de098ef107
Lanielle Pavlik <70656068+LanielleP@users.noreply.github.com>
2021-10-23 19:57:53 +0000

    There are a lot more methods in PlanB now to break the sentence-building into smaller steps. Most of the sentences generated are 3 or 5 words long.

--- PlanB.java
+++ PlanB.java
@@ -8,57 +8,88 @@ public class PlanB
   public static ArrayList<String> nouns;
   public static ArrayList<String> verbs;
   public static ArrayList<String> articles;
+  public static ArrayList<String> adjectives;//not used yet
+  public static ArrayList<String> adverbs;//not used yet
+  public static ArrayList<String> prepositionalPhrases;//not used yet
+  public static ArrayList<String> conjunctions;//not used yet
     
   public static void main(/*String[] args*/)
   {
-    articles = readFile("Word Bank/articles.txt");
-    nouns = readFile("Word Bank/nouns.txt");
-    verbs = readFile("Word Bank/verbs.txt");
-
-    String article = getWord(articles);
-    String subject = getWord(nouns);
-    String verb = getWord(verbs);
-    
-    if(article.equals("a") && isVowel(subject))article = "an";
-    if(verb.substring(verb.length()-1).equals("y"))verb = verb.substring(0,verb.length()-1)+"ies";
-    else verb+="s";
-
-    String sentence = article+" "+subject+" "+verb+". ";
-    sentence = sentence.substring(0,1).toUpperCase()+sentence.substring(1).toLowerCase();
-
-    System.out.print(sentence);
+    setUpLists();
+    System.out.print(buildSentence());
   }//ends main
 
   public static ArrayList<String> readFile(String file)
   {
     ArrayList<String> info = new ArrayList<String>();
     
-    try
-    {
+    try{
       Scanner fileReader = new Scanner(new File(file));
       while(fileReader.hasNext()) info.add(fileReader.next());
       fileReader.close();
-    }//ends try file stuff
-    catch (IOException e)
-    {
+    }catch (IOException e){
       System.out.println("Something's wrong with the file.");
     }//ends catch file errors
 
-    
     return info;
   }
+
+  public static String buildSentence()
+  {
+    String sentence = "";
+    sentence += fullNoun(getWord(articles),getWord(nouns))+" ";
+    sentence += fullVerb(getWord(verbs));
+    //sentence += fullVerb("frown");
+
+    if(sentence.substring(sentence.length()-2).equals("at") || Math.random()>0.5)sentence+=" "+fullNoun(getWord(articles),getWord(nouns));
+
+    return formatSentence(sentence);
+  }//builds a sentence
 
   public static String getWord(ArrayList<String> list)
   {
     return list.get((int)(Math.random()*list.size()));
   }//ends getNoun() method
 
+  private static void setUpLists()
+  {
+    articles = readFile("Word Bank/articles.txt");
+    nouns = readFile("Word Bank/nouns.txt");
+    verbs = readFile("Word Bank/verbs.txt");
+  }//ends setUpLists method
+
   private static boolean isVowel(String str)
   {
     return str.substring(0,1).toLowerCase().matches("[aeiou]");    
   }//ends isVowel
 
+  private static String fullNoun(String art, String n)
+  {
+    String phrase = "";
+    if(art.equals("a")) phrase+=(isVowel(n))?"an":"a";
+    else phrase+=art;
+    //ADJECTIVES WILL GO HERE
+    return phrase+" "+n;
+  }//ends fullNoun method - changes A to An if neccessary, returns article+noun, when we want to add adjectives, they go here
+
+  private static String fullVerb(String v)
+  {
+    String result = v.substring(0,v.length()-1);
+    if(v.substring(v.length()-1).equals("y")) result+="ies";
+    else if(v.substring(v.length()-1).equals("s")) result+="es";
+    else result+=v.substring(v.length()-1)+"s";
+    //ADVERBS and any other words that need to follow the verb (ex. "at" for the verb "looked at") WILL GO HERE
+    if (v.equals("frown"))result+=" at";
+    return result;
+  }//returns complete, conjugated verb
+
+  private static String formatSentence(String s)
+  {    
+    return s.substring(0,1).toUpperCase()+s.substring(1).toLowerCase()+".";
+  }//ends formatSentence - puts a capital letter and a period
+
 }//ends PlanB class 
+
 
 //https://replit.com/talk/learn/Get-started-with-Web-Scraping/8930
 //https://replit.com/@jajoosam/quote-scraper-starter
