d64250e1be78f1744e0a80dc0ccc4e6787c2efe6
Lanielle Pavlik <70656068+LanielleP@users.noreply.github.com>
2021-10-27 17:23:54 +0000

    Updated Test

--- Test.java
+++ Test.java
@@ -1,13 +1,51 @@
 import java.util.*;
+import java.io.IOException;//for file issues
+import java.io.File;//used to read file
+import java.io.FileWriter;//used to save file
 
-public class Test 
+public class Test
 {
-  public static void main(String[] args) 
+  public static void main(String[] args)
   {
-    for (int i = 0; i < 10; i++) 
+    //FIX LATER
+    Object obj = new Object();
+    Subject sbj = new Subject();
+    VerbsAndAdverbs vAa = new VerbsAndAdverbs();
+    
+    for(int i=0;i<10;i++) System.out.println(SentenceBuilder.getSentence());
+  }//ends main method
+
+  /***
+   * returns the input from a given file as an ArrayList
+   * @param String file
+   * @returns ArrayList<String>
+   */
+  public static ArrayList<String> readFile(String file)
+  {
+    ArrayList<String> info = new ArrayList<String>();
+    
+    try{
+      Scanner fileReader = new Scanner(new File(file));
+      while(fileReader.hasNext()) info.add(fileReader.next());
+      fileReader.close();
+    }catch (IOException e){
+      System.out.println("Something's wrong with the file.");
+    }//ends catch file errors
+
+    return info;
+  }//ends readFile
+
+  public static String getWord(ArrayList<String> list)
+  {
+    try{ return list.get((int)(Math.random()*list.size()));}
+    catch(NullPointerException e)
     {
-      SentenceBuilder.main();
-      System.out.println();
-    } // ends for loop
-  }// ends main method
-}// ends test class
+      return "_";
+    }//ends catch
+  }//ends getNoun() method
+
+  public static boolean isVowel(String str)
+  {
+    return str.substring(0,1).toLowerCase().matches("[aeiou]");    
+  }//ends isVowel
+}//ends test class
