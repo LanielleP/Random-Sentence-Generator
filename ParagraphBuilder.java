db5da2bf09a4a77c44696469e998336e657f8300
Jomar Jocson <70657450+JomarJocson@users.noreply.github.com>
2021-10-27 05:56:21 +0000

    Mode + FileCreation (User's means to input own word bank will be implemented later)

--- ParagraphBuilder.java
+++ ParagraphBuilder.java
@@ -5,6 +5,39 @@ import java.io.FileWriter;//used to save file
 
 public class ParagraphBuilder{
   
+  public static void createFile(String[] args){
+    try {
+      File ownWordBank = new File("UserWordBank.txt");
+      if (ownWordBank.createNewFile()) {
+        System.out.println("File created: " + ownWordBank.getName());
+      } else {
+        System.out.println("File already exists.");
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }//ends try catch
+  }//ends file creation method
 
-  
-}//https://www.w3schools.com/java/java_files_create.asp
+public static int mode()
+  {
+    Scanner userMode = new Scanner(System.in);
+    int mode = -1;
+    while (mode == -1){
+    System.out.println("Please choose 0 - control; 1 - autonomy");
+    mode = Integer.valueOf(userMode.nextLine());   
+    if (mode == 0){
+
+    }
+    else if (mode == 1){
+
+    }
+    else{
+      System.out.println("Unvalid Input");
+    }
+
+    }//ends while loop
+    return mode;
+  }//User takes control or srg is autonomous
+
+}//ends ParagraphBuilder class
