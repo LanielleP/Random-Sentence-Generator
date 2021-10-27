9cbb6a78a5130d0b3d772dc6fd530302759a9d4e
Lanielle Pavlik <70656068+LanielleP@users.noreply.github.com>
2021-10-27 17:21:27 +0000

    Might have updated SentenceBuilder

--- SentenceBuilder.java
+++ SentenceBuilder.java
@@ -3,141 +3,68 @@ import java.io.IOException;//for file issues
 import java.io.File;//used to read file
 import java.io.FileWriter;//used to save file
 
-public class SentenceBuilder 
+public class SentenceBuilder
 {
-  public static ArrayList<String> nouns;
-  public static ArrayList<String> verbs;
-  public static ArrayList<String> articles;
-  public static ArrayList<String> adjectives;// not used yet
-  public static ArrayList<String> adverbs;// not used yet
-  public static ArrayList<String> prepositionalPhrases;// not used yet
-  public static ArrayList<String> conjunctions;// not used yet
+  public static ArrayList<String> prepositionalPhrases;//not used yet
+  public static ArrayList<String> conjunctions;//not used yet
 
-  public static void main(/* String[] args */) 
-  {
-    setUpLists();
-    System.out.print(buildSentence());
-  }// ends main
-
-  private static void setUpLists() 
-  {
-    articles = readFile("Word Bank/articles.txt");
-    nouns = readFile("Word Bank/nouns.txt");
-    verbs = readFile("Word Bank/verbs.txt");
-  }// ends setUpLists method
-
-  public static ArrayList<String> readFile(String file) 
-  {
-    ArrayList<String> info = new ArrayList<String>();
-
-    try 
-    {
-      Scanner fileReader = new Scanner(new File(file));
-      while (fileReader.hasNext())
-        info.add(fileReader.next());
-      fileReader.close();
-    } 
-    catch (IOException e) 
-    {
-      System.out.println("Something's wrong with the file.");
-    } // ends catch file errors
-
-    return info;
-  }
-
-  public static String indepdentClause() 
+  private static String indepdentClause()
   {
     String independent = "";
     return independent;
-  }// Code to build Independent Clause
+  }//Code to build Independent Clause
 
-  public static String dependentClause() 
+  private static String dependentClause()
   {
     String dependent = "";
     return dependent;
-  }// Code for Dependent Clause
+  }//Code for Dependent Clause
 
-  public static int sentenceTense() 
+  public static int sentenceTense()
   {
     Scanner se = new Scanner(System.in);
     int tense = -1;
     int valid = -1;
-    while (valid == -1) 
-    {
+    while(valid == -1){
       System.out.println("Please choose 0 - past; 1 - present, 2 - future");
-      tense = Integer.valueOf(se.nextLine());
-      if (tense == 0) 
+      tense = Integer.valueOf(se.nextLine());   
+      if(tense == 0)
       {
         valid++;
-      } 
-      else if (tense == 1) 
+      }
+      else if (tense == 1)
       {
         valid++;
-      } 
-      else if (tense == 2) 
+      }
+      else if (tense == 2)
       {
         valid++;
-      } 
-      else 
-      {
+      }
+      else{
         System.out.println("Unvalid Input");
       }
-    } // ends while loop
-    return tense;
-  }// ends sentenceTense method
+    }//ends while loop
 
-  public static String buildSentence() 
+    return tense;
+  }//ends sentenceTense method
+
+  public static String getSentence()
   {
     String sentence = "";
-    sentence += fullNoun(getWord(articles), getWord(nouns), "subject") + " ";
-    sentence += VerbsAndAdverbs.conjugateVerb(getWord(verbs), 0);
-    // sentence += VerbsAndAdverbs.conjugateVerb("convey");
+    sentence += Subject.getSubject()+" ";
+    sentence += VerbsAndAdverbs.conjugateVerb();
+    //sentence += VerbsAndAdverbs.conjugateVerb("convey");
 
-    if (sentence.substring(sentence.length() - 3, sentence.length() - 2).equals(" ") || Math.random() > 0.1)
-      sentence += " " + fullNoun(getWord(articles), getWord(nouns), "object");
+    if(sentence.substring(sentence.length()-3,sentence.length()-2).equals(" ") || Math.random()>0.1)sentence+=" "+Object.getObject();
 
     return formatSentence(sentence);
-  }// builds a sentence
+  }//builds a sentence
 
-  public static String getWord(ArrayList<String> list) 
-  {
-    return list.get((int) (Math.random() * list.size()));
-  }// ends getNoun() method
+  private static String formatSentence(String s)
+  {    
+    return s.substring(0,1).toUpperCase()+s.substring(1).toLowerCase()+".";
+  }//ends formatSentence - puts a capital letter and a period
 
-  
+  //make alternate formatSentence that takes in an ArrayList
 
-  private static boolean isVowel(String str) 
-  {
-    return str.substring(0, 1).toLowerCase().matches("[aeiou]");
-  }// ends isVowel
-
-  private static String fullNoun(String art, String n, String type) 
-  {
-    String phrase = "";
-    if (Math.random() * 100 > 15) 
-    {
-      if (art.equals("a"))
-        phrase += (isVowel(n)) ? "an" : "a";
-      else
-        phrase += art;
-      // ADJECTIVES WILL GO HERE
-      return phrase + " " + n;
-    } 
-    else 
-    {
-      if (type.equals("subject"))
-        return "it";
-      else
-        return "them";
-      // To do: make conjugation match other possible pronouns (ex. "they") and plural
-      // nouns
-    }
-  }// ends fullNoun method - changes A to An if neccessary, returns article+noun,
-   // when we want to add adjectives, they go here
-
-  private static String formatSentence(String s) 
-  {
-    return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase() + ".";
-  }// ends formatSentence - puts a capital letter and a period
-
-}// ends SentenceBuilder class
+}//ends SentenceBuilder class 
